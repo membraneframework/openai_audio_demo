f3c36312f5556f4150e6bd74e472272b367c26ad
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2024-10-09 12:06:02 +0200

    wip

--- examples/demo.exs
+++ examples/demo.exs
@@ -49,10 +49,13 @@ defmodule OpenAIElement do
 
   @impl true
   def handle_buffer(:input, buffer, _ctx, state) do
-    # buffer preprocessing
-    dbg(buffer, label: "SENT BUFFER")
-    frame = Jason.encode(%{type: "input_audio_buffer.append", audio: buffer.payload})
+    audio = Base.encode64(buffer.payload)
+    frame = %{type: "input_audio_buffer.append", audio: audio} |> Jason.encode!()
     :ok = OpenAIWebSocket.send_frame(state.ws, frame)
+
+    # frame = Jason.encode!(%{type: "input_audio_buffer.commit"})
+    # :ok = OpenAIWebSocket.send_frame(state.ws, frame)
+
     {[], state}
   end
 
@@ -76,16 +79,18 @@ defmodule OpenAIElement do
 
   defp create_response(ws) do
     # frame = Jason.encode(%{type: "input_audio_buffer.commit"})
-    frame = Jason.encode(%{type: "response.create"})
-    :ok = OpenAIWebSocket.send_frame(ws, frame)
+    # frame = Jason.encode!(%{type: "response.create"})
+    # :ok = OpenAIWebSocket.send_frame(ws, frame)
   end
 
   @impl true
-  def handle_info({:websocket_frame, frame}, _ctx, state) do
-    decoded_frame = Jason.decode!(frame)
-    dbg(decoded_frame, label: "DECODED FRAME")
-    # {[], state}
-    {[buffer: {:output, %Membrane.Buffer{payload: decoded_frame.audio}}], state}
+  def handle_info({:websocket_frame, {:text, frame}}, _ctx, state) do
+    with %{"type" => "response.audio.delta", "delta" => delta} <- Jason.decode!(frame) |> dbg(label: "DECODE") do
+      audio_payload = Base.decode64!(delta)
+      {[buffer: {:output, %Membrane.Buffer{payload: audio_payload}}], state}
+    else
+      %{} -> {[], state}
+    end
   end
 end
 
@@ -103,13 +108,15 @@ defmodule BrowserToOpenAi do
       |> child(:opus_decoder, %Membrane.Opus.Decoder{sample_rate: 24_000})
       |> child(:open_ai, %OpenAIElement{websocket_opts: opts[:openai_ws_opts]})
       |> child(:raw_audio_parser, Membrane.RawAudioParser)
-      |> child(:opus_encoder, Membrane.Opus.Encoder)
-      |> via_in(:input, options: [kind: :audio])
-      |> child(:webrtc_sink, %Membrane.WebRTC.Sink{
-        signaling: {:websocket, port: opts[:webrtc_source_ws_port]}
-      })
+      |> child(:portaudio_sink, Membrane.PortAudio.Sink)
 
-    self() |> Process.send_after(:create_response, 10_000)
+    # |> child(:opus_encoder, Membrane.Opus.Encoder)
+    # |> via_in(:input, options: [kind: :audio])
+    # |> child(:webrtc_sink, %Membrane.WebRTC.Sink{
+    #   signaling: {:websocket, port: opts[:webrtc_sink_ws_port]}
+    # })
+
+    # self() |> Process.send_after(:create_response, 10_000)
 
     {[spec: spec], %{}}
   end
@@ -127,6 +134,10 @@ end
 
 openai_api_key = System.get_env("OPENAI_API_KEY")
 
+if openai_api_key == nil do
+  raise "You have to set OPENAI_API_KEY env"
+end
+
 openai_ws_opts = [
   extra_headers: [
     {"Authorization", "Bearer " <> openai_api_key},
@@ -137,14 +148,15 @@ openai_ws_opts = [
 {:ok, _supervisor, pipeline} =
   Membrane.Pipeline.start_link(BrowserToOpenAi,
     openai_ws_opts: openai_ws_opts,
-    webrtc_ws_port: 8829
+    webrtc_source_ws_port: 8829,
+    webrtc_sink_ws_port: 8830
   )
 
 {:ok, _server} =
   :inets.start(:httpd,
     bind_address: ~c"localhost",
     port: 8000,
-    document_root: ~c"#{__DIR__}/assets/browser_to_file",
+    document_root: ~c"#{__DIR__}/assets",
     server_name: ~c"webrtc",
     server_root: "/tmp"
   )
@@ -154,3 +166,5 @@ Process.monitor(pipeline)
 receive do
   {:DOWN, _ref, :process, ^pipeline, _reason} -> :ok
 end
+
+# The server's responses timing depends on the turn_detection configuration (set with session.update after a session is started):
